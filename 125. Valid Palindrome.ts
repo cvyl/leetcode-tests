e4ddb0dd8c4bfa1ac1b8fedbc395274eaa0b8c76
跨性别 <cvyl@pm.me>
2024-03-12 11:52:26 +0100

    feat: New Code

---
+++ 125. Valid Palindrome.ts
@@ -0,0 +1,7 @@
+function isPalindrome(s: string): boolean {
+    // Remove non-alphanumeric characters and convert to lowercase
+    const alphanumeric = s.replace(/[^a-z0-9]/gi, '').toLowerCase();
+    // Compare the string with its reverse
+    return alphanumeric === alphanumeric.split('').reverse().join('');
+    
+};
